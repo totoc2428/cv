1f2c8188f01717d855a25dd88de3d0d3bfadebb6
Charles Coudé <charles.coude.etu@univ-lille.fr>
2025-02-28 23:47:06 +0100

    correction css

--- src/view/about/aboutView.tsx
+++ src/view/about/aboutView.tsx
@@ -38,7 +38,7 @@ export class AboutView extends React.Component<{}, AboutViewState> {
     return (
       <section className="about-container">
         <Header title="about" />
-        <main className="main main-about">
+        <main className="main about-section">
           <div className="about-title">
             <img src={profileImage} alt="about" className="profile-image" />
             <h2>
